aba7ef98df474cf6978a04960e74091d8392ad96
thomas-j92 <thomas.j@impactus.co.uk>
2017-05-13 17:13:20 +0100

    spaceship modifications

--- js/main.js
+++ js/main.js
@@ -5,9 +5,16 @@ var player_canvas;				// used to store canvas
 var e_ctx;						// context - 2d
 var b_ctx;						// context - 2d
 var p_ctx;						// context - 2d
+
+// ship settings
 var ship;						// stores ship details
+var ship_width 			= 60;   // width of ship
+var ship_height 		= 15;   // height of ship
+var nose_width 			= 12; 	// width of ship nose
+var nose_height 		= 35;   // height of ship nose
 var ship_speed 			= 10;	// stores speed of ship
 var bullet_speed 		= 4;	// how fast ship fires
+
 var elements 			= []; 	// all elements on screen (ships - lasers are stored seperately)
 var lasers 				= [];  	// all lasers on screen
 var lasers_max			= 3;  	// how many lasers they can fire at once - keeps page speeds down
@@ -55,17 +62,15 @@ function startGame() {
   	updateLaserCount(lasers_available);
 
   	// create your ship
-	var ship_width 		= 30; // width of ship
-	var ship_height 	= 10; // height of ship
 	var window_spacing 	= 10; // how much space should be between ship & screen
 	var starting_x 		= window_spacing;
 	var starting_y 		= (p_ctx.canvas.height - ship_height) - window_spacing; // minus height + spacing - otherwise it'll show below users viewport
 	ship 				= new Ship(starting_x, starting_y, ship_width, ship_height, 1, '000', true); // create Ship object
 	elements.push(ship); // add to array - used by drawCanvas to draw all objects
 
-	setUpAnimations();
+	//setUpAnimations();
 
-	setUpLevel();
+	//setUpLevel();
 
 	drawCanvas(); // wipes canvas clean and re-populates using various arrays (var lasers, elements etc)
 }
@@ -210,8 +215,74 @@ function drawShip(element) {
 	p_ctx.lineWidth	= element.border;
 
 	if(element.fill) { // fill element with a color if fill set to true
+		// ship body
 		p_ctx.fillStyle = "#"+ element.color;
 		p_ctx.fillRect(element.x,element.y,element.width,element.height);
+
+		// make nose of ship
+		var nose_x 		= element.x + (element.width/2) - (nose_width/2);
+		var nose_y 		= element.y - nose_height;
+		p_ctx.fillRect(nose_x, nose_y, nose_width, nose_height);
+
+		// make nose rounded
+		var radius = nose_width/2;
+		var rounded_x = nose_x + (nose_width/2);
+		var rounded_y = nose_y;
+		p_ctx.beginPath();
+		p_ctx.arc(rounded_x, rounded_y, radius, 0, 2 * Math.PI, false);
+		p_ctx.fill();
+
+		// left shooter connection of wings
+		var connect_padding = 3;
+		var connect_x 		= element.x - connect_padding;
+		var connect_y 		= element.y - connect_padding;
+		var connect_width	= 6;
+		var connect_height 	= element.height + (connect_padding);
+		p_ctx.strokeStyle 	= 'gray';
+		p_ctx.lineWidth		= 2;
+		p_ctx.strokeRect(connect_x, connect_y, connect_width, connect_height); // use to create outline 
+		p_ctx.fillRect(connect_x, connect_y, connect_width, connect_height);
+
+		// left laser shooter
+		var shooter_width 	= 3;
+		var shooter_height 	= element.height + (nose_height*0.75);
+		var shooter_x 		= connect_x + (connect_width/2) - (shooter_width/2);
+		var shooter_y 		= nose_y + (nose_height * 0.25);
+		p_ctx.fillRect(shooter_x, shooter_y, shooter_width, shooter_height);
+		// left shooter detail
+		var detail_indent 	= 1;
+		var detail_width 	= shooter_width + (detail_indent*2);
+		var detail_height 	= 2;
+		var detail_x 		= shooter_x - detail_indent;
+		var detail_y 		= shooter_y + (shooter_height*0.15);
+		p_ctx.fillRect(detail_x, detail_y, detail_width, detail_height);
+		
+		// right connection
+		connect_x = element.x + element.width - connect_padding;
+		p_ctx.strokeRect(connect_x, connect_y, connect_width, connect_height); // use to create outline 
+		p_ctx.fillRect(connect_x, connect_y, connect_width, connect_height);
+
+		// right shooter
+		shooter_x = element.x + element.width - (shooter_width/2);
+		p_ctx.fillRect(shooter_x, shooter_y, shooter_width, shooter_height);
+		// right shooter detail
+		detail_x = shooter_x - detail_indent;
+		p_ctx.fillRect(detail_x, detail_y, detail_width, detail_height);
+
+		// left engine
+		var engine_width 	= 7;
+		var engine_padding 	= (nose_height*0.1);
+		var engine_height 	= element.height + (engine_padding*2);
+		var engine_x 		= nose_x - engine_width;
+		var engine_y 		= element.y - engine_padding;
+		p_ctx.strokeRect(engine_x, engine_y, engine_width, engine_height); // use to create outline 
+		p_ctx.fillRect(engine_x, engine_y, engine_width, engine_height);
+
+		// right engine
+		engine_x = nose_x + nose_width;
+		p_ctx.strokeRect(engine_x, engine_y, engine_width, engine_height); // use to create outline 
+		p_ctx.fillRect(engine_x, engine_y, engine_width, engine_height);
+
 	} else { //otherwise, dont fill
 		p_ctx.strokeRect(element.x,element.y,element.width,element.height);
 	}
@@ -320,12 +391,12 @@ function drawCanvas() {
 		var element = $(this)[0];
 		drawShip(element);
 	});
-	$.each(lasers, function() {
-		var element = $(this)[0];
-		drawLaser(element);
-	});
-	$.each(enemies, function() {
-		var element = $(this)[0];
-		element.draw();
-	});		
+	// $.each(lasers, function() {
+	// 	var element = $(this)[0];
+	// 	drawLaser(element);
+	// });
+	// $.each(enemies, function() {
+	// 	var element = $(this)[0];
+	// 	element.draw();
+	// });		
 }
